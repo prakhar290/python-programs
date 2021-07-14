a2440aac7265d2e93b0a7e252167cd88fd266ad5
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-14 23:08:32 +0530

    Add files via upload

---
+++ Creating Variables.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 2+2 #valid variable
+print(x)
+
+
+# In[2]:
+
+
+1x = 2+2 #invalid variable
+print(1x)
+
+
+# In[3]:
+
+
+x@ = 2+2 #invalid variable
+print(x@)
+
+
+# In[5]:
+
+
+x_num = 2+2 #valid variable as underscore is the only special character which is allowed in naming variable
+print(x_num)
+
+
+# In[6]:
+
+
+X = 2+3 #this is different from x as it is in uppercase and python have a difference between lowercase and uppercase
+print(X)
+
+
+# In[ ]:
+
+
+
+
